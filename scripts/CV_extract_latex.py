9af75c187d5916a48f75a6debdc2081af3079090
Hanno Kase <kase.hanno@gmail.com>
2021-03-17 18:33:50 +0100

    Fixed paths to files

--- scripts/CV_extract_latex.py
+++ scripts/CV_extract_latex.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 
-input = "./public/print/index.html"
+input = "./print/index.html"
 print()
 # Extract relevant part of the page
 soup = BeautifulSoup(open(input), 'html.parser')
@@ -20,6 +20,6 @@ for htmlesc, latexesc in escape_characters.items():
     CV_tex_clean = CV_tex_clean.replace(htmlesc, latexesc)
 
 # Write the output to tex file
-file = open('./static/files/CV_Hanno_Kase.tex', 'w')
+file = open('./files/CV_Hanno_Kase.tex', 'w')
 file.write(CV_tex_clean)
 file.close()
